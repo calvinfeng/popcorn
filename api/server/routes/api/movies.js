8d6bc7eb6cc1274b40f2e6dc66cf04326b1f5bba
Carmen To <carmen.j.to@gmail.com>
2019-01-28 21:13:17 -0800

    Created multiple movie related endpoints

--- api/server/routes/api/movies.js
+++ api/server/routes/api/movies.js
@@ -23,11 +23,15 @@ router.get('/details/:imdbId', async (req, res) => {
         const tmdbRes = await axios.get(`https://api.themoviedb.org/3/movie/${imdbId}?api_key=${apiKey}`);
         const date = new Date();
         await pool.query('INSERT INTO movie_details (imdb_id, created_at, updated_at, detail) VALUES ($1, $2, $3, $4)', [imdbId, date, date, tmdbRes.data]);
+        
+        res.setHeader('Content-Type', 'application/json');
         res.status(200).send(tmdbRes.data);
-      } catch (error) {
-        console.log(error)
+      } 
+      catch (err) {
         const status = error.response.status;
-        const message = `There is no movie associated with the provided IMDB ID: ${imdbId}.`;
+        const message = `The movie with the given IMDB ID ${imdbId} was not found: ${err}`;
+        
+        res.setHeader('Content-Type', 'application/json');
         res.status(status).send(message);
       }
     } else {
@@ -36,11 +40,75 @@ router.get('/details/:imdbId', async (req, res) => {
     }
   } 
   catch(err) {
-    console.log('error ', err);    
+    res.status(404).send(`The movie with the given IMDB ID ${imdbId} was not found: ${err}`);    
   }
   finally {
     client.release();
   }
 });
+
+router.get('/:imdbId', async (req, res) => {
+  let client;
+  const imdbId = req.params.imdbId;
+  try {
+    client = await pool.connect();
+    const { rows } = await pool.query('SELECT * From movies WHERE imdb_id = $1', [imdbId]);
+    let movie = rows[0];
+    
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(movie);
+  }
+  catch {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(404).send(`The movie with the given IMDB ID ${imdbId} was not found: ${err}`)
+  }
+  finally {
+    client.release();
+  }
+});
+
+router.get('/page/', async (req, res) => {
+  let client;
+  try {
+    client = await pool.connect();
+  }
+  catch {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(404).send(`The page was not found: ${err}`
+  }
+  finally {
+    client.release();
+  }
+})
+
+router.post('/rate/:imdbId', async (req, res) => {
+  let client;
+  const imdbId = req.params.imdbId;
+  try {
+    client = await pool.connect();
+  }
+  catch {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(404).send(`The movie with the given IMDB ID ${imdbId} was not found: ${err}`)
+  }
+  finally {
+    client.release();
+  }
+})
+
+router.put('/rate/:imdbId', async (req, res) => {
+  let client;
+  const imdbId = req.params.imdbId;
+  try {
+    client = await pool.connect();
+  }
+  catch {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(404).send(`The movie with the given IMDB ID ${imdbId} was not found: ${err}`
+  }
+  finally {
+    client.release();
+  }
+})
 
 module.exports = router;
