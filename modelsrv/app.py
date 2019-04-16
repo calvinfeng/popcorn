892303a977eefc7944ddf11d6794b4ceec3feb13
Calvin Feng <calvinjhfeng@gmail.com>
2019-04-16 16:43:24 -0700

    Model serving with Flask is not bad after all, the next step is to try to set up gRPC server in Python

--- modelsrv/app.py
+++ modelsrv/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, render_template
 from flask import request
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
 import numpy as np
 import cv2
 
 
 app = Flask(__name__)
+model = ResNet50(weights='imagenet')
 
 
 @app.route('/', methods=['GET'])
@@ -26,10 +28,20 @@ def classify():
         return resp
 
     np_data = cv2.imdecode(np.fromstring(bytestr, np.uint8), cv2.IMREAD_COLOR)
-    resp = jsonify(np_data.reshape(-1, *np_data.shape).shape)
+    np_data = cv2.resize(np_data, (224, 224))
+    x = np_data.reshape(-1, *np_data.shape)
+    x = preprocess_input(x)
+    preds = model.predict(x)
+    preds = decode_predictions(preds, top=3)[0]
+
+    classes = []
+    for pred in preds:
+        classes.append(pred[1])
+
+    resp = jsonify({ 'classes': classes })
     resp.status_code = 200
     return resp
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=False, threaded=False, port=8080)
